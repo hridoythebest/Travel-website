080cc5553ad3a93fe073ec691f9400f8227a4a73
hridoythebest <hridoythebest@gmail.com>
2024-02-08 06:41:29 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Home from './components/Home'
+import Places from './components/Places'
+import Discover from './components/Discover'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Places/>
+      <Discover/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
